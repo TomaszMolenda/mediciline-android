04cc0fd61143fc79948620e149cfa19ec1f87ad6
tomo <tomasz.molenda@gmail.com>
2016-05-31 22:31:22 +0200

    Add alarmManager (to schedulted synchronie db)

---
+++ app/src/main/java/local/tomo/login/service/Alarm.java
@@ -0,0 +1,46 @@
+package local.tomo.login.service;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.PowerManager;
+import android.util.Log;
+import android.widget.Toast;
+
+/**
+ * Created by tomo on 2016-05-28.
+ */
+public class Alarm extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent)
+    {
+        PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+        PowerManager.WakeLock wl = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "");
+        wl.acquire();
+
+        // Put here YOUR code.
+        Log.d("tomo", "co 1 sek");
+        Toast.makeText(context, "Alarm !!!!!!!!!!", Toast.LENGTH_LONG).show(); // For example
+
+        wl.release();
+    }
+
+    public void SetAlarm(Context context)
+    {
+        AlarmManager am =( AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+        Intent i = new Intent(context, Alarm.class);
+        PendingIntent pi = PendingIntent.getBroadcast(context, 0, i, 0);
+        am.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), 1000*20, pi); // Millisec * Second * Minute
+    }
+
+    public void CancelAlarm(Context context)
+    {
+        Intent intent = new Intent(context, Alarm.class);
+        PendingIntent sender = PendingIntent.getBroadcast(context, 0, intent, 0);
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        alarmManager.cancel(sender);
+    }
+}
---
+++ app/src/main/java/local/tomo/login/service/SynchronizeService.java
@@ -0,0 +1,36 @@
+package local.tomo.login.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+/**
+ * Created by tomo on 2016-05-28.
+ */
+public class SynchronizeService extends android.app.Service {
+
+    Alarm alarm = new Alarm();
+    public void onCreate()
+    {
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId)
+    {
+        alarm.SetAlarm(this);
+        return START_STICKY;
+    }
+
+    @Override
+    public void onStart(Intent intent, int startId)
+    {
+        alarm.SetAlarm(this);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent)
+    {
+        return null;
+    }
+}
