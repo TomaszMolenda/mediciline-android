60dd97d7994b2579f14cd1b20b5f9cfe14886005
tomo <tomasz.molenda@gmail.com>
2016-06-13 22:13:23 +0200

    remove two byttons from medicaments list view

--- app/src/main/java/local/tomo/login/AllMedicamentsFragment.java
+++ app/src/main/java/local/tomo/login/AllMedicamentsFragment.java
@@ -4,8 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -17,6 +19,7 @@ import java.util.List;
 import local.tomo.login.database.DatabaseHandler;
 import local.tomo.login.model.Medicament;
 import local.tomo.login.network.RestIntefrace;
+import local.tomo.login.network.Synchronize;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Call;
@@ -33,6 +36,7 @@ public class AllMedicamentsFragment extends Fragment {
 
     private AllMedicamentAdapter allMedicamentAdapter;
     private ListView listView ;
+    private SwipeRefreshLayout swipeRefreshLayout;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -40,26 +44,36 @@ public class AllMedicamentsFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.all_medicaments_layout, container, false);
         databaseHandler = new DatabaseHandler(getContext(), null, null, 1);
         medicaments = databaseHandler.getMedicaments();
-        Log.d("tomo", "ile w liscie przed" + medicaments.size());
+
+        swipeRefreshLayout = (SwipeRefreshLayout) rootView.findViewById(R.id.swipe_refresh_layout);
 
         listView = (ListView) rootView.findViewById(android.R.id.list);
         allMedicamentAdapter = new AllMedicamentAdapter(getContext(), R.layout.all_medicament_list_row, (ArrayList<Medicament>) medicaments);
 
         listView.setAdapter(allMedicamentAdapter);
 
-
-        Button buttonDeleteMedicaments = (Button) rootView.findViewById(R.id.buttonDeleteMedicaments);
-        Button buttonSynchronizeMedicaments = (Button) rootView.findViewById(R.id.buttonSynchronizeMedicaments);
         FloatingActionButton fabAddMedicament = (FloatingActionButton) rootView.findViewById(R.id.fabAddMedicament);
-        buttonDeleteMedicaments.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                databaseHandler.removeAllMedicaments();
-                medicaments.clear();
-                allMedicamentAdapter.notifyDataSetChanged();
 
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                swipeRefreshLayout.setRefreshing(true);
+
+                Log.d("tomo", "srart ref");
+                Synchronize synchronize = new Synchronize(getContext());
+                synchronize.synchronizeAllMedicaments(swipeRefreshLayout, allMedicamentAdapter);
             }
         });
+
+//        buttonDeleteMedicaments.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                databaseHandler.removeAllMedicaments();
+//                medicaments.clear();
+//                allMedicamentAdapter.notifyDataSetChanged();
+//
+//            }
+//        });
 
         fabAddMedicament.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -69,91 +83,6 @@ public class AllMedicamentsFragment extends Fragment {
             }
         });
 
-        buttonSynchronizeMedicaments.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
-                interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
-                OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
-
-                Retrofit retrofit = new Retrofit.Builder()
-                        .baseUrl(RestIntefrace.url)
-                        .addConverterFactory(GsonConverterFactory.create())
-                        .client(client)
-                        .build();
-                RestIntefrace restIntefrace = retrofit.create(RestIntefrace.class);
-                Call<List<Medicament>> call = restIntefrace.getMedicaments(LoginActivity.uniqueId);
-                call.enqueue(new Callback<List<Medicament>>() {
-                    @Override
-                    public void onResponse(Call<List<Medicament>> call, Response<List<Medicament>> response) {
-                        List<Medicament> medicaments = response.body();
-                        Log.d("tomo", "w liscie: " + medicaments.size());
-                        Log.d("tomo", "internet");
-                        List<Medicament> medicamentsLocal = databaseHandler.getMedicaments();
-
-                        endloop:
-                        for (Medicament medicament : medicaments) {
-                            if(!medicamentsLocal.contains(medicament)) {
-                                for (Medicament medicamentLocal : medicamentsLocal) {
-                                    if(medicament.getIdServer() == medicamentLocal.getIdServer()) {
-                                        databaseHandler.updateMedicament(medicament.getIdServer(), medicament);
-                                        break endloop;
-                                    }
-                                }
-                                databaseHandler.addMedicament(medicament);
-                            }
-
-
-//                            Log.d("tomo", medicament.toString());
-//                            Log.d("tomo", String.valueOf(medicament.hashCode()));
-
-
-
-                        }
-                        List<Medicament> medicamentsToSend = databaseHandler.getMedicamentsToSend();
-                        Log.d("tomo", "local");
-                        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
-                        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
-                        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
-                        Retrofit retrofit = new Retrofit.Builder()
-                                .baseUrl(RestIntefrace.url)
-                                .addConverterFactory(GsonConverterFactory.create())
-                                .client(client)
-                                .build();
-                        RestIntefrace restIntefrace = retrofit.create(RestIntefrace.class);
-
-                        for (final Medicament medicament : medicamentsToSend) {
-                            Log.d("tomo", medicament.toString());
-                            Call<Medicament> medicamentCall = restIntefrace.saveMedicament(medicament);
-                            medicamentCall.enqueue(new Callback<Medicament>() {
-                                @Override
-                                public void onResponse(Call<Medicament> call, Response<Medicament> response) {
-                                    Log.d("tomo", "poszlo medi");
-                                    Medicament body = response.body();
-                                    Log.d("tomo", body.toString());
-                                    medicament.setIdServer(body.getId());
-                                    boolean b = databaseHandler.setIdServer(medicament);
-                                    Log.d("tomo", "czy sie udlo" + b);
-
-                                }
-
-                                @Override
-                                public void onFailure(Call<Medicament> call, Throwable t) {
-                                    Log.d("tomo", "nie poszlo medi synchro");
-                                    //databaseHandler.addMedicament(medicamentDb, date);
-                                }
-                            });
-                        }
-                    }
-
-                    @Override
-                    public void onFailure(Call<List<Medicament>> call, Throwable t) {
-
-                    }
-                });
-
-            }
-        });
         return rootView;
     }
 
