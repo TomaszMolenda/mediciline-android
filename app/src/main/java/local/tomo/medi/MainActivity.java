bf5a2d1e47a1fbd0e0b55e553aa26463d01d0b87
tomo <tomasz.molenda@gmail.com>
2016-07-10 14:32:23 +0200

    double tap to close app

--- app/src/main/java/local/tomo/medi/MainActivity.java
+++ app/src/main/java/local/tomo/medi/MainActivity.java
@@ -6,13 +6,17 @@ import android.app.Activity;
 
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
+import android.widget.Toast;
 
 
 import local.tomo.medi.swipe.SwipeFragment;
 
 public class MainActivity extends FragmentActivity {
+
+    FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +24,36 @@ public class MainActivity extends FragmentActivity {
         setContentView(R.layout.activity_main);
 
 
-        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager = getSupportFragmentManager();
         fragmentManager.beginTransaction()
                 .replace(R.id.content_frame, new SwipeFragment())
                 .commit();
+
+    }
+
+    //http://stackoverflow.com/a/13578600
+    boolean doubleBackToExitPressedOnce = false;
+
+    @Override
+    public void onBackPressed() {
+        if(fragmentManager.getBackStackEntryCount() != 0) {
+            fragmentManager.popBackStack();
+            return;
+        } else {
+            if (doubleBackToExitPressedOnce) {
+                super.onBackPressed();
+                return;
+            }
+        }
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "Proszę kilknąć cofnij jeszcze raz aby zamknąć program", Toast.LENGTH_SHORT).show();
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce=false;
+            }
+        }, 2000);
 
     }
 }
