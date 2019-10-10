d1543c97c516b21a4f19a423cf1fcbcef4e949bf
tmolenda <tomasz.molenda@thecoders.io>
2019-10-10 09:18:38 +0200

    fix multiple start activity

--- app/src/main/java/local/tomo/medi/activity/drug/scan/ScanActivity.java
+++ app/src/main/java/local/tomo/medi/activity/drug/scan/ScanActivity.java
@@ -2,10 +2,11 @@ package local.tomo.medi.activity.drug.scan;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.SparseArray;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
-import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
@@ -13,9 +14,6 @@ import com.google.android.gms.vision.CameraSource;
 import com.google.android.gms.vision.Detector;
 import com.google.android.gms.vision.barcode.Barcode;
 import com.google.android.gms.vision.barcode.BarcodeDetector;
-
-import java.io.IOException;
-import java.util.Objects;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -40,21 +38,24 @@ public class ScanActivity extends DatabaseAccessActivity {
         setContentView(R.layout.activity_scan);
         ButterKnife.bind(this);
 
-        BarcodeDetector barcodeDetector = new BarcodeDetector.Builder(this).build();
+        BarcodeDetector barcodeDetector = new BarcodeDetector.Builder(ScanActivity.this).build();
 
-        cameraSource = new CameraSource.Builder(this, barcodeDetector)
+        cameraSource = new CameraSource.Builder(ScanActivity.this, barcodeDetector)
                 .setAutoFocusEnabled(true)
                 .build();
 
         cameraView.getHolder().addCallback(new SurfaceHolder.Callback() {
+
+            @SneakyThrows
             @Override
             public void surfaceCreated(SurfaceHolder holder) {
 
-                try {
-                    cameraSource.start(cameraView.getHolder());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+                cameraSource = new CameraSource
+                        .Builder(ScanActivity.this, barcodeDetector)
+                        .setAutoFocusEnabled(true)
+                        .build();
+
+                cameraSource.start(cameraView.getHolder());
             }
 
             @Override
@@ -85,22 +86,16 @@ public class ScanActivity extends DatabaseAccessActivity {
 
                     Drug drug = getHelper().getDrugQuery().getEan(ean);
 
-                    if (Objects.isNull(drug)) {
+                    Intent intent = new Intent(ScanActivity.this, SetOverdueActivity.class);
 
-                        String message = getString(R.string.ean_not_found, ean);
+                    Bundle bundle = new Bundle();
+                    bundle.putInt(D_ID, drug.getId());
 
-                        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+                    intent.putExtras(bundle);
+                    startActivity(intent);
 
-                    } else {
-
-                        Intent intent = new Intent(getApplicationContext(), SetOverdueActivity.class);
-
-                        Bundle bundle = new Bundle();
-                        bundle.putInt(D_ID, drug.getId());
-
-                        intent.putExtras(bundle);
-                        startActivity(intent);
-                    }
+                    Handler handler = new Handler(Looper.getMainLooper());
+                    handler.post(() -> cameraSource.stop());
                 }
             }
         });
